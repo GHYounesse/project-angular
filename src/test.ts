15a740f71a21d7ecd29e5623a7c00c35ab8e7efb
instutmoselle <instututmosselle@gmail.com>
2026-02-08 13:32:52 +0100

    adding CI pipline 8

---
+++ src/test.ts
@@ -0,0 +1,18 @@
+// This file is required by karma and loads recursively all the .spec.ts files
+import 'zone.js';
+import 'zone.js/testing';
+import { getTestBed } from '@angular/core/testing';
+import {
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting,
+} from '@angular/platform-browser-dynamic/testing';
+
+declare const require: any;
+
+getTestBed().initTestEnvironment(
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting()
+);
+
+const context = require.context('./', true, /\.spec\.ts$/);
+context.keys().map(context);
